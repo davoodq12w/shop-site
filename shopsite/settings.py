6eb4578b54f1ac78d4139833922ac398192f552f
davoodq12w <145224799+davoodq12w@users.noreply.github.com>
2024-06-16 19:30:52 +0330

    Update settings.py

--- shopsite/settings.py
+++ shopsite/settings.py
@@ -84,26 +84,13 @@ WSGI_APPLICATION = 'shopsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# ================================================================
-#                       for data bace
-# ================================================================
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'shop',
-        'USER': 'davood',
-        'PASSWORD': '12345678',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
