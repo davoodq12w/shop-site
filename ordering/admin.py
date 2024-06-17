2a97825ff70f3bc78c3272b85e042e439195cc9d
davood <davod.q12w@gmail.com>
2024-06-17 12:17:10 +0330

    update project

--- ordering/admin.py
+++ ordering/admin.py
@@ -2,7 +2,30 @@ from django.contrib import admin
 from .models import Order, OrderItem
 import openpyxl
 from django.http import HttpResponse
+import csv
 # Register your models here.
+
+
+def export_csv(modeladmin, request, queryset):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=Orders.csv'
+
+    writer = csv.writer(response)
+
+    columns = ['ID', 'Name', 'Phone', 'Address', 'Postal Code',
+               'Province', 'City', 'Paid', 'Created']
+    writer.writerow(columns)
+
+    for order in queryset:
+        created = str(order.created.replace(tzinfo=None))
+        writer.writerow(
+            [order.id, order.name, order.phone, order.address, order.postal_code,
+             order.province, order.city, order.paid, created]
+        )
+    return response
+
+
+export_csv.short_description = 'Export to CSV'
 
 
 def export_excel(modeladmin, request, queryser):
@@ -42,4 +65,4 @@ class OrderAdmin(admin.ModelAdmin):
                     'paid', 'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
-    actions = [export_excel]
+    actions = [export_excel, export_csv]
